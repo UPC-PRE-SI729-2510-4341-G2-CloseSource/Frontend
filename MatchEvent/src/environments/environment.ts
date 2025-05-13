7264733e6ae591583abe454805e36497a4336e27
MaxPP <u201314454@upc.edu.pe>
2025-05-13 13:56:07 -0500

    feat (environments): add environment

---
+++ MatchEvent/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+// Server Base Path for Fake REST API
+  serverBasePath: 'http://localhost:3000/api/v1',
+// Server Base Path for Spring Boot REST API
+//serverBasePath: 'http://localhost:8090/api/v1',
+  milestonesEndpointPath: '/milestones'
+};
